c25b6d186a08dd34109a2a2c4b1a8a954626381e
Rutuja-1710 <rutujalokhande268@gmail.com>
2026-09-24 16:15:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-
+const visitCounter = require("./Middleware/visitcounter");
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
+
+
+app.use(visitCounter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Mini Project!");
+});
+
+app.get("/visits", (req, res) => {
+  const data = require("fs").readFileSync("./visits.json", "utf8");
+  res.json(JSON.parse(data));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
 
